233e36d294d6a8585eaee8c42b120b1b7fe96eaa
CeeWy <princyunlimited@gmail.com>
2023-07-22 21:50:00 +0100

    Function for fibonacci

---
+++ python-functions/5-prime.py
@@ -0,0 +1,13 @@
+def is_prime(n):
+    if n > 1:
+	    for i in range(2, int(n/2)+1):
+		    if (n % i) == 0:
+			    print(n, "is not a prime number")
+		    break
+	    else:
+		    print(n, "is a prime number")
+# If the number is less than 1, its also not a prime number.
+    else:
+	    print(n, "is not a prime number")
+	    
+print(is_prime(9))
