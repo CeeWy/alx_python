cfac6b8f240a0cc3c4c15b0d4aed2425d39d7434
CeeWy <princyunlimited@gmail.com>
2023-07-22 20:04:25 +0100

    Final touches

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 #Write a program that prints numbers from 0 to 99
 for i in range(10):
-    for j in range(10):
-        if(i==j):
-            print("")
+    for j in range(i + 1, 10):
+        if(i == 8 and j == 9):
+            print("{}{}" .format(i, j))
         else:
-            print("{}{}" .format(i,j) , end=", ")
+            print("{}{}" .format(i, j) , end=", ")
