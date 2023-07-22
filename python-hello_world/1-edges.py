fe5fa0243db7df5cb735b52436a53c8909edfca3
CeeWy <princyunlimited@gmail.com>
2023-07-22 14:28:36 +0100

    1. Copy - Cut - Paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: {}".format(hol))
+print("Last 2 letters: {}".format(on))
+print("Middle word: {}".format(olberton))
