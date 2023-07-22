095138e78c1cd52eaa6c7051f303afa010301800
CeeWy <princyunlimited@gmail.com>
2023-07-22 14:41:42 +0100

    More changes

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,3 @@
-word = "Holberton"
-print("First 3 letters: {}".format("Hol"))
-print("Last 2 letters: {}".format("on"))
-print("Middle word: {}".format("olberto"))
 Word = "School"
 print("First 3 letters: {}".format("Sch"))
 print("Last 2 letters: {}".format("ol"))
