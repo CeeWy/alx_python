f8274b76cc2e38e4a92e6576ae2e4bb9d6d6b391
CeeWy <princyunlimited@gmail.com>
2023-07-23 17:18:36 +0100

    Password validator

---
+++ python-functions/6-password.py
@@ -0,0 +1,23 @@
+def validate_password(password):
+    if len(password) > 8:
+        return False
+    if ' ' in password:
+        return False
+    # Check for uppercase, lowercase, and digits
+    upperCase = False
+    lowerCase = False
+    digit = False
+
+    for char in password:
+        if char.isupper():
+            upperCase = True
+        elif char.islower():
+            lowerCase = True
+        elif char.isdigit():
+            digit = True
+    if upperCase and lowerCase and digit:
+        return True
+    else:
+        return False
+    
+    print(validate_password(perinc1))
