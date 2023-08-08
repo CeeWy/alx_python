e0c5ccc60e389a496b9bac28e569cc617e8ef3f2
CeeWy <princyunlimited@gmail.com>
2023-08-08 01:57:59 +0100

    changes

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 import sys
 
-def arguments():
-    num_arguments = len(sys.argv)
 
-    # Print the number of arguments
-    if num_arguments == 1: 
-        print("Number of arguments:{}." .format(num_arguments))
+def main():
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    if num_args > 0:
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
+
+
+if __name__ == "__main__":
+    main()
--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    raise NameError(This is an error message)
+    raise NameError(message)
